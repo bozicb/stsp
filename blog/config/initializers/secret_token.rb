6463071b7145df41c7b193eb2d81c44093a3d7a2
Bojan Božić <bojan.bozic@ait.ac.at>
2014-08-26 17:09:45 +0200

    creation

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '74a2c05f3490f96483a854d6fb550a223a6247efa5ce870a44861aeecc00946b692c1ddaf1f1e6861fd0e677624407c7a6c9220c3315265fa055453446bc8081'
